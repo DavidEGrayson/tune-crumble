cc98c5313bfe5f2d2b95a9476617476a09269c2f
David Grayson <davidegrayson@gmail.com>
2013-11-09 15:36:59 -0800

    remembered to add util.js

---
+++ util.js
@@ -0,0 +1,64 @@
+function getDisplayPathOrNull(entry, callback)
+{
+  if (entry)
+  {
+    chrome.fileSystem.getDisplayPath(entry, callback)
+  }
+  else
+  {
+    callback(null)
+  }
+}
+
+function getTimestampOrNull(entry, callback)
+{
+  if (entry)
+  {
+    entry.getMetadata(function(metadata)
+    {
+      callback(metadata.modificationTime);
+    },
+    function(err)
+    {
+      console.log("error getting metadata for entry")
+      console.log(entry)
+      console.log(err)
+      callback(null)
+    });
+  }
+  else
+  {
+    callback(null)
+  }
+}
+
+function getDisplayPathList(entries, callback)
+{
+  entries.mapWithCallback(chrome.fileSystem.getDisplayPath, callback)
+}
+
+Array.prototype.mapWithCallback = function(mapper, callback)
+{
+  var result = []
+  var resultCount = 0
+  
+  if(this.length == 0)
+  {
+    callback([])
+    return;
+  }
+  
+  for(var i = 0; i < this.length; i++)
+  {
+    (function(i){
+      mapper(this[i], function(mapped)
+      {
+        result[i] = mapped
+        if (++resultCount == this.length)
+        {
+          callback(result)
+        }
+      }.bind(this))
+    }).call(this, i)
+  }
+}
